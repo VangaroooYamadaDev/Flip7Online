5e96108f97fffb0d04172a9557e1de90290b4414
Vangarooo <vangarooo.yamada+github@gmail.com>
2026-04-01 09:29:46 +0900

    update CardDefinition
    add constructors for Multiplier and Special cards

--- Assets/Scripts/Data/CardDefinition.cs
+++ Assets/Scripts/Data/CardDefinition.cs
@@ -2,9 +2,26 @@ using UnityEngine;
 
 public enum CardType
 {
-    Number,         // 数字カード
-    Multiplier,     // 倍率カード
-    Special         // 特殊カード
+    Number,         // number card
+    Multiplier,     // multiplier card
+    Special         // special card
+}
+
+public enum MultiplierType
+{
+    PlusTwo,
+    PlusFour,
+    PlusSix,
+    PlusEight,
+    PlusTen,
+    TimesTwo
+}
+
+public enum SpecialType
+{
+    Freeze,
+    FlipThree,
+    SecondChance
 }
 
 public class CardDefinition
@@ -12,11 +29,30 @@ public class CardDefinition
     public string Name { get; }
     public CardType Type { get; }
     public int? Number { get; }
+    public MultiplierType MultiplierType { get; }
+    public SpecialType SpecialType { get; }
 
+    // constructor for number cards
     public CardDefinition(string name, CardType type, int? number = null)
     {
         Name = name;
         Type = type;
         Number = number;
+    }
+
+    // constructor for multiplier cards
+    public CardDefinition(string name, MultiplierType multiplier)
+    {
+        Name = name;
+        Type = CardType.Multiplier;
+        MultiplierType = multiplier;
+    }
+
+    // constructor for number cards
+    public CardDefinition(string name, SpecialType special)
+    {
+        Name = name;
+        Type = CardType.Special;
+        SpecialType = special;
     }
 }
