17d8bc07f4a936f57b2a37878891ee9dbc29544b
Vangarooo <vangarooo.yamada+github@gmail.com>
2026-04-02 17:49:04 +0900

    add GameManager script

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private List<Player> _players = new List<Player>();
+    private Deck _deck;
+    private int _currentPlayerIndex;
+    private Dictionary<Player, int> _scores = new Dictionary<Player, int>();
+
+    [SerializeField]
+    private int _playerCount = 3;
+
+    private void Start()
+    {
+        InitializeGame();
+    }
+
+    private void InitializeGame()
+    {
+        _deck = new Deck();
+
+        for (int i = 0; i < _playerCount; i++)
+        {
+            var Player = new Player($"Player{i + 1}");
+            _players.Add(Player);
+            _scores[player] = 0;
+        }
+
+        _currentPlayerIndex = 0;
+    }
+
+    void Update()
+    {
+
+    }
+}
